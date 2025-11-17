7ef18d84a305dabbb55090d3e2dc3784b9f946a9
Marijus_Mozdeikis <marijus.mozdeikis@ff.stud.vu.lt>
2025-11-17 16:57:38 +0200

    Find peaks by properties

--- scripts/Analyze_dips_template.py
+++ scripts/Analyze_dips_template.py
@@ -2,7 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import glob
 import os   
-from peakutils import baseline
+from scipy.signal import find_peaks
+import seaborn as sns
 
 
 folder = r"C:\Users\Marijus\OneDrive - Vilnius University\3 semestras\cern\cern_data_analysis_project\20250722_05nJ_06-10um_Spectras"
@@ -12,8 +13,26 @@ df = pd.read_excel(file, sheet_name="Calibration 1-Measurements")
 
 wavelength_col = df.columns[0]
 s_cols = [c for c in df.columns if c.endswith(" S ")]
-df[s_cols] = -df[s_cols]
+# Invert the signal data
+df[s_cols] = df[s_cols].max()-df[s_cols]
+signal = df[s_cols[0]]
 
+# Peak detection parameters
+threshold = 0.05 
+min_distance = 10
 
-plt.plot(df[wavelength_col], df[s_cols[0]], label = s_cols[0])
+peaks, properties = find_peaks(signal.values, prominence=threshold, distance=min_distance)
+
+# Plot original signal
+plt.figure(figsize=(10,5))
+plt.plot(df[wavelength_col], signal, label='Inverted signal')
+
+# Overlay peaks as red dots
+plt.scatter(df[wavelength_col].iloc[peaks],
+            signal.values[peaks],
+            color='red', s=50, label='Detected peaks')
+
+plt.xlabel('Wavelength (nm)')
+plt.ylabel('Reflection (inverted)')
+plt.legend()
 plt.show()
